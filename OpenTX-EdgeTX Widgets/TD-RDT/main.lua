b7cf3ef65fde171e1eb542ca69f5f372f8184251
i3dm <liorthebest@hotmail.com>
2024-07-23 14:12:28 +0300

    Add files via upload

---
+++ OpenTX-EdgeTX Widgets/TD-RDT/main.lua
@@ -0,0 +1,5 @@
+-------------------------------------------------------------------------------
+--  /WIDGETS/MY_WIDGET/main.lua
+-------------------------------------------------------------------------------
+print('/WIDGETS/MY_WIDGET/main.lua')
+return loadScript("/WIDGETS/MY_WIDGET/TD-RDT.luac") ()
