ce08a2f3138297daf4176c8b2cde3084791d5edb
i3dm <8968780+i3dm@users.noreply.github.com>
2024-09-03 11:59:51 +0300

    Delete OpenTX-EdgeTX Widgets/TD-RDT directory

--- OpenTX-EdgeTX Widgets/TD-RDT/main.lua
+++
@@ -1,5 +0,0 @@
--------------------------------------------------------------------------------
---  /WIDGETS/MY_WIDGET/main.lua
--------------------------------------------------------------------------------
-print('/WIDGETS/MY_WIDGET/main.lua')
-return loadScript("/WIDGETS/MY_WIDGET/TD-RDT.luac") ()
